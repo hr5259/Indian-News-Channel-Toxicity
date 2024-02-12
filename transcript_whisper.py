304b0dae5d8462c78f58ffc7a02d7999968d5264
Rushang Gala <rg9851@rit.edu>
2024-02-12 12:58:01 -0500

    Updated the transcript whisper code to generate the transcripts for significant event videos.

--- transcript_whisper.py
+++ transcript_whisper.py
@@ -1,19 +1,23 @@
 import whisper
 import os
-import pandas as pd
 
+def transcript_generator(audio_path,file_name):
+    model = whisper.load_model("base")
+    # Transcribe the audio
+    result = model.transcribe(audio_path)
+    file_name_without_extension = os.path.splitext(file_name)[0] + '.txt'
+    output_path = os.path.join("transcripts", file_name_without_extension)
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.write(result['text'])
 
+def main():
+    folder_path = 'downloaded_audios'
+    file_names = os.listdir(folder_path)
+    for file_name in file_names:
+        print("Generating transcript for: ",file_name)
+        # Path to your audio file
+        audio_path = os.path.join("downloaded_audios", file_name)
+        transcript_generator(audio_path, file_name)
 
-# Path to your audio file
-audio_path = os.path.join("downloaded_audios", "Watch_ Viewpoint With Bhupendra Chaubey.mp3")
-print(audio_path)
-model = whisper.load_model("base")
-
-# Transcribe the audio
-result = model.transcribe(audio_path)
-
-output_path = os.path.join("transcripts", "transcript.txt")
-
-# Access the transcript and other information
-with open(output_path, "w", encoding="utf-8") as f:
-    f.write(result['text'])
+if __name__ == '__main__':
+    main()
